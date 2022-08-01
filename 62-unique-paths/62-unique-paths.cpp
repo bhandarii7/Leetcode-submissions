1b8ae54d1a0dde4ada6c6ad7968723a7933e836e
yash bhandari <71130152+bhandarii7@users.noreply.github.com>
2022-08-01 17:56:51 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -16,7 +16,24 @@ public:
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return fn(m-1,n-1,dp);
+        vector<vector<double>>dp(m+1,vector<double>(n+1,0));
+        
+        for(int i = 0;i<=m;i++)
+            dp[i][0] = 1;
+        
+        for(int j=0;j<=n;j++) dp[0][j] = 1;
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                double left = dp[i][j-1];
+                double up = dp[i-1][j];
+                
+                dp[i][j] = left+up;
+            }
+        }
+        // return fn(m-1,n-1,dp);
+        return  (int)dp[m-1][n-1];
     }
 };

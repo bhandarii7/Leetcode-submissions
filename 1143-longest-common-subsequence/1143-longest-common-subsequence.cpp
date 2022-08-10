42698a71603f1ae7c19a76def6632cfa625d015e
yash bhandari <71130152+bhandarii7@users.noreply.github.com>
2022-08-11 02:55:07 +0530

    Time: 84 ms (5.55%), Space: 13.3 MB (11.09%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int fn(int i, int j, string &s1, string &s2,vector<vector<int>>&dp)
+    {
+        if(i<0 || j<0) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s1[i]==s2[j])
+            return 1+fn(i-1,j-1,s1,s2,dp);
+        
+        int left = fn(i-1,j,s1,s2,dp);
+        int right = fn(i,j-1,s1,s2,dp);
+        
+        return dp[i][j] =  max(left,right);
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        
+        return fn(n-1,m-1,text1,text2,dp);
+    }
+};

ff2c29177fd9f5d67ac7c342adc1d0b61981f733
yash bhandari <71130152+bhandarii7@users.noreply.github.com>
2022-08-11 02:59:17 +0530

    Time: 47 ms (31.51%), Space: 13.1 MB (22.07%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -19,8 +19,25 @@ public:
         int n = text1.size();
         int m = text2.size();
         
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         
-        return fn(n-1,m-1,text1,text2,dp);
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                {
+                    
+                    int left = dp[i-1][j];
+                    int right = dp[i][j-1];
+
+                     dp[i][j] =  max(left,right);
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 };
